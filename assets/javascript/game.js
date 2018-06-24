03c355e5721c21e9d86f15e142280e3b6021d855
Win Le <nguyenwinle@gmail.com>
2018-06-23 20:33:47 -0700

    update game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,44 +18,93 @@ $(document).ready(function() {
         // set the key to Userguess
         var userGuess = event.key;
 
+        // letters only
+ 
         // when the user hits a key
             // decrement total guesses
             // add the key to the empty array
             // add our guesses and guesses to our html
-        if (userGuess) {
-            guesses--;
-            guessLetters.push(userGuess);
-            document.querySelector(".guessleft").innerHTML = guesses;
-            document.querySelector(".yourguesses").innerHTML = guessLetters;
+        if (/^[a-zA-Z]/.test(userGuess) && userGuess.length === 1) {
+            if (userGuess) {
+                guesses--;
+                guessLetters.push(userGuess);
+                document.querySelector(".guessleft").innerHTML = guesses;
+
+                for (var i = 0; i < userGuess.length; i++) {
+                    $(".yourguesses").append('<div class = "col-md-1">' + userGuess + '</div>');
+                    //document.querySelector(".col-md-1").innerHTML = guessLetters;
+                }
+
+            }
+
+            // when the users guess is the same as the random letter
+                // increment win count
+                // then reset everything except our losses and wins
+            if (userGuess === randomLetter) {
+                winCount = winCount + 1;
+                document.querySelector(".guessleft").innerHTML = "10";
+                guesses = 10;
+                document.querySelector(".yourguesses").innerHTML = "";
+                guessLetters = [];
+                document.querySelector(".win").innerHTML = winCount;
+                alert("You win!");       
+            }
+
+            // when the guesses reach 0
+                // decrement losses and add it to our html
+                // reset everything except losses and wins
+            if (guesses === 0) {
+                lossCount++;
+                document.querySelector(".loss").innerHTML = lossCount;
+                alert("You lose!");
+                document.querySelector(".guessleft").innerHTML = "10";
+                guesses = 10;
+                document.querySelector(".yourguesses").innerHTML = "";
+                guessLetters = [];  
+                 
+            }
+        } else {
+            alert("Invalid guess!");
         }
 
-        // when the users guess is the same as the random letter
-            // increment win count
-            // then reset everything except our losses and wins
-        if (userGuess === randomLetter) {
-            winCount = winCount + 1;
-            document.querySelector(".guessleft").innerHTML = "10";
-            guesses = 10;
-            document.querySelector(".yourguesses").innerHTML = "";
-            guessLetters = [];
-            document.querySelector(".win").innerHTML = winCount;
-            alert("You win!");       
+        if (guessLetters.length === 0) {
+            $(".guesses").css("width", "100%");
         }
 
-        // when the guesses reach 0
-            // decrement losses and add it to our html
-            // reset everything except losses and wins
-        if (guesses === 0) {
-            alert("Game Over!");
-            lossCount++;
-            document.querySelector(".loss").innerHTML = lossCount;
-            document.querySelector(".guessleft").innerHTML = "10";
-            guesses = 10;
-            document.querySelector(".yourguesses").innerHTML = "";
-            guessLetters = [];   
+        if (guessLetters.length === 1) {
+            $(".guesses").css("width", "90%");
         }
+        if (guessLetters.length === 2) {
+            $(".guesses").css("width", "80%");
+        }
+        if (guessLetters.length === 3) {
+            $(".guesses").css("width", "70%");
+        }
+        if (guessLetters.length === 4) {
+            $(".guesses").css("width", "60%");
+        }
+        if (guessLetters.length === 5) {
+            $(".guesses").css("width", "50%");
+        }
+        if (guessLetters.length === 6) {
+            $(".guesses").css("width", "40%");
+        }
+        if (guessLetters.length === 7) {
+            $(".guesses").css("width", "30%");
+        }
+
+        if (guessLetters.length === 8) {
+            $(".guesses").css("width", "20%");
+        }
+        if (guessLetters.length === 9) {
+            $(".guesses").css("width", "10%");
+        }
+
+        if (guessLetters.length === 10) {
+            $(".guesses").css("width", "0%");
+        } 
+
     }
+
+
 });
-
-
-computerLetters.includes(UserInput);
